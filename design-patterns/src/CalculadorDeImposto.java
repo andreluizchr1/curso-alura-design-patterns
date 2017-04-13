21f99f1da2394e3f3f63ba6418fca9e352743fef
André Luiz <andreluizchr1@gmail.com>
2017-04-13 18:14:36 -0300

    Remoção dos ifs no calculo do imposto

--- design-patterns/src/CalculadorDeImposto.java
+++ design-patterns/src/CalculadorDeImposto.java
@@ -1,17 +1,16 @@
 
 public class CalculadorDeImposto {
 
-	public void realizaCalculo(Orcamento orcamento, String imposto) {
+	public void realizaCalculoICMS(Orcamento orcamento, String imposto) {
 
-		if (imposto.equals("imposto")) {
-
-			double icms = orcamento.getValor() * 0.1;
+			double icms = new ICMS().calculaICMS(orcamento);
 			System.out.println(icms);
 
-		} else if (imposto.equals("ICSS")) {
-			double iss = orcamento.getValor() * 0.6;
-			System.out.println(iss);
-		}
-
+	}
+	
+	public void realizaCalculoISS(Orcamento orcamento){
+		
+		double iss = new ISS().calculaISS(orcamento);
+		System.out.println(iss);
 	}
 }
