ffbdd53711af3bdc7a026371fe25706cb8d6e29e
André Luiz <andreluizchr1@gmail.com>
2017-04-13 20:23:04 -0300

    Inclusão da classe Calculadora de Desconto

---
+++ design-patterns/src/CalculadorDeDesconto.java
@@ -0,0 +1,13 @@
+
+public class CalculadorDeDesconto {
+
+	public double calcula(Orcamento orcamento) {
+		if (orcamento.getItens().size() > 5) {
+			return orcamento.getValor() * 0.1;
+		} else if (orcamento.getValor() > 500.0) {
+			return orcamento.getValor() * 0.07;
+		}
+
+		return 0;
+	}
+}
